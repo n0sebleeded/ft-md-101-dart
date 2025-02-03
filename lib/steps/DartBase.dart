d49a83eaabce12844cc5f6bc0fc4c4f7f17d712c
n0sebleeded <devDanilAkhunzianov@outlook.com>
2025-02-04 01:13:02 +0300

    Lesson #2 -- Data Types

--- lib/steps/DartBase.dart
+++ lib/steps/DartBase.dart
@@ -1,5 +1,25 @@
 class DartBase {
   void run() {
     print("DartBase: hello world");
+    _stepConf();
+    _stepDataType();
+  }
+
+  void _stepConf() {
+    print("_stepConf: This is step 1");
+  }
+
+  void _stepDataType() {
+    String str = "string";
+    int intVal = 1;
+    double doubleVal = 1.1;
+    double sum = intVal + doubleVal;
+    DartBase db = DartBase();
+
+    print("_stepDataType: $str");
+    print("_stepDataType: $intVal");
+    print("_stepDataType: $doubleVal");
+    print("_stepDataType: $sum");
+    db._stepConf();
   }
 }
