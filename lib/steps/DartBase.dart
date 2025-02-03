e8ed34c3afdf79f3fa37ad7fc236ef974dd8cec8
n0sebleeded <devDanilAkhunzianov@outlook.com>
2025-02-04 01:57:22 +0300

    Lesson #5 -- Methods

--- lib/steps/DartBase.dart
+++ lib/steps/DartBase.dart
@@ -1,4 +1,5 @@
 import 'package:ft_md_101_dart/ClassInstance.dart';
+import 'package:ft_md_101_dart/steps/utils/DartMethod.dart';
 import 'package:ft_md_101_dart/steps/utils/DartPackage.dart';
 
 class DartBase {
@@ -36,5 +37,10 @@ class DartBase {
   void _stepPackage() {
     DartPackage dp = DartPackage();
     dp.run();
+  }
+
+  void _stepMethod() {
+    DartMethod dm = DartMethod();
+    dm.run();
   }
 }
